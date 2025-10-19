71a578c6bf84684a86b84360e2461d20982ec955
Cyprians Mwanza <cypriansmwanza@outlook.com>
2025-10-19 17:35:32 +0300

    hive helper

---
+++ lib/services/local/hive_helper.dart
@@ -0,0 +1,37 @@
+import 'package:hive/hive.dart';
+import '../../models/note.dart';
+
+class HiveHelper {
+  static const String _boxName = 'notesBox';
+
+  Future<void> init() async {
+    if (!Hive.isBoxOpen(_boxName)) {
+      await Hive.openBox<Note>(_boxName);
+    }
+  }
+
+  Future<List<Note>> getNotes() async {
+    final box = Hive.box<Note>(_boxName);
+    return box.values.toList();
+  }
+
+  Future<void> addNote(Note note) async {
+    final box = Hive.box<Note>(_boxName);
+    await box.put(note.id, note);
+  }
+
+  Future<void> updateNote(Note note) async {
+    final box = Hive.box<Note>(_boxName);
+    await box.put(note.id, note);
+  }
+
+  Future<void> deleteNoteById(String id) async {
+    final box = Hive.box<Note>(_boxName);
+    await box.delete(id);
+  }
+
+  Future<void> clearAll() async {
+    final box = Hive.box<Note>(_boxName);
+    await box.clear();
+  }
+}
